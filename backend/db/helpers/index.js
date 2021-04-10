299d28f815ec9f6623d37dc4dc8fabc8810f45af
souredoutlook <souredoutlook@gmail.com>
2021-04-10 01:33:36 -0400

    add getUserById
    Co-authored-by: Amazigh-SR <s_Red009@hotmail.com>

--- backend/db/helpers/index.js
+++ backend/db/helpers/index.js
@@ -12,6 +12,21 @@ const addUser = function (twitter_id, username, email, token, secret_token, imag
     VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING *;
   `;
-  return db.query(queryString, queryParams).then((res) => res.rows)
+  return db.query(queryString, queryParams).then((res) => res.rows[0])
 };
 exports.addUser = addUser;
+
+/**
+ * Query db by twitter_id
+ * @param number
+ * @returns the new user id.
+ */
+ const getUserById = function (twitter_id) {
+  queryParams = [twitter_id];
+  queryString = `
+    SELECT * FROM users
+    WHERE twitter_id = $1;
+  `;
+  return db.query(queryString, queryParams).then((res) => res.rows[0])
+};
+exports.getUserById = getUserById;
