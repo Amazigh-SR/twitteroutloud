d7c4d45bdbce14b293cad0d14aa71e72a7b2b91f
souredoutlook <souredoutlook@gmail.com>
2021-04-10 01:09:40 -0400

    adjust schema to include image_url
    Co-authored-by: Amazigh-SR <s_Red009@hotmail.com>

--- backend/db/00_schema/01_users.sql
+++ backend/db/00_schema/01_users.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL,
-  twitter_id INTEGER PRIMARY KEY,
+  twitter_id BIGINT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   token VARCHAR(255),
   secret_token VARCHAR(255),
-  verified BOOLEAN
+  image_url VARCHAR(255)
 );
