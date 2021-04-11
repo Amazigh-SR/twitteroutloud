3ba4bfe962ff9ec98b07ea597d7eb51950a487b6
souredoutlook <souredoutlook@gmail.com>
2021-04-11 01:50:56 -0400

    implement axios call to DELETE /session Co-authored-by: Amazigh-SR <s_Red009@hotmail.com> Co-authored-by: Dan Pappo <dpappo@gmail.com>

--- MyApp/src/components/Header.jsx
+++ MyApp/src/components/Header.jsx
@@ -1,6 +1,18 @@
-import Cookies from "js-cookie";
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function Header(props) {
+
+  const deleteSession = function() {
+    console.log("I am delet")
+    axios.delete('http://localhost:3001/session', {withCredentials: true, headers: {"Access-Control-Allow-Origin": "http://localhost:3000/"}})
+    .then(res => {
+      console.log(res)
+      props.setUserAccess(false);
+    })
+    .catch(err => console.error(err))
+  };
+
   return (<nav>
     <h1 className="nav">#TwitterOutLoud 🔊</h1>
     
@@ -8,7 +20,7 @@ export default function Header(props) {
 
     {!props.userAccess && <a href="http://localhost:3001/auth"><img src="../../sign-in-with-twitter-link.png" alt="Sign in with Twitter" /></a>}
 
-    {props.userAccess && <a href="http://localhost:3000"><button onClick={Cookies.remove("userAccess")} className="btn btn-primary logout">Logout 👋</button></a>}
+    {props.userAccess && <button onClick={()=>deleteSession()} className="btn btn-primary logout">Logout 👋</button>}
 
     {console.log("props.userAccess: ", props.userAccess)}
 
