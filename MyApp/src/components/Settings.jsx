5edc0ba49d8e83aeb8bb79760e9dd8d7972bdb43
Dan Pappo <dpappo@gmail.com>
2021-04-11 15:59:18 -0400

    add done button to settings modal component, and change steps

--- MyApp/src/components/Settings.jsx
+++ MyApp/src/components/Settings.jsx
@@ -22,12 +22,12 @@ export default function Settings(props) {
 
   <div class="option">
 		<label for="voice">Voice</label>
-		<select name="voice" id="voice"></select>
+		<select name="voice" id="voice" onChange={handleChange}></select>
 	</div>
 
 	<div class="option">
 		<label for="volume">Volume</label>
-		<input type="range" min="0" max="1" step="0.1" name="volume" id="volume" value={settings.volume} onChange={handleChange}/>
+		<input type="range" className="form-range" min="0" max="1" step="0.01" name="volume" id="volume" value={settings.volume} onChange={handleChange}/>
 	</div>
 
 	<div class="option">
@@ -37,8 +37,11 @@ export default function Settings(props) {
 
 	<div class="option">
 		<label for="pitch">Pitch</label>
-		<input type="range" min="0" max="2" step="0.1" name="pitch" id="pitch" value={settings.pitch} onChange={handleChange}/>
+		<input type="range" min="0" max="2" step="0.01" name="pitch" id="pitch" value={settings.pitch} onChange={handleChange}/>
 	</div>
+
+  <button className="btn player"
+      onClick={""}>Done</button>
 
   </div>
 }
