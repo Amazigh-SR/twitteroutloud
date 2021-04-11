9b4f570bcf62ec66266af0c24c6c9c3cf0c532e6
Amazigh-SR <s_Red009@hotmail.com>
2021-04-11 12:56:32 -0400

    Implement fetchEnVoices helper function
    Co-authored-by: souredoutlook <souredoutlook@gmail.com>
    Co-authored-by: Dan Pappo <dpappo@gmail.com>

---
+++ MyApp/src/helpers/fetchEnVoices.js
@@ -0,0 +1,11 @@
+//Function 2 - a function that fetches all the EN voices (from a total of 67) that the API has to offer
+const fetchEnVoices = function (synthesis) {
+  const englishRegex = /^en(-[a-z]{2})?$/i; // Regex to match all types of en e.g. en-US, en-CA etc.
+  const voices = synthesis
+    .getVoices()
+    .filter((voice) => englishRegex.test(voice.lang)); // returns all the english speaking voices
+
+  return voices;
+};
+
+module.exports = fetchEnVoices;
