7ca7edaea6a7a58f15bee6cfee1cfb1a9855bed0
Amazigh-SR <s_Red009@hotmail.com>
2021-04-11 12:36:23 -0400

    Implement text to speech functionality
    Co-authored-by: Dan Pappo <dpappo@users.noreply.github.com>
    Co-authored-by: Nicholas Meisenheimer <souredoutlook@users.noreply.github.com>

--- MyApp/src/App.js
+++ MyApp/src/App.js
@@ -1,42 +1,49 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import Auth from "./components/Auth";
 import Header from "./components/Header";
 import Pull from "./components/Pull";
 import TweetList from "./components/TweetList";
+import Speech from "./components/Speech";
 
 function App() {
-  console.log(process.env.REACT_APP_FRONT_END_HOST)
-  const isLoggedIn = localStorage.getItem('isLoggedIn') || false;
-  
-  const [userAccess, setUserAccess] = useState(isLoggedIn === "true" ? true : false);
-  
-  useEffect(() =>{
-    if (!isLoggedIn){
-      axios.get(`${process.env.REACT_APP_BACK_END_HOST}/validate`, {withCredentials: true, headers: {"Access-Control-Allow-Origin": process.env.REACT_APP_FRONT_END_HOST}})
-        .then(res => {
-          console.log(res.data)
+  console.log(process.env.REACT_APP_FRONT_END_HOST);
+  const isLoggedIn = localStorage.getItem("isLoggedIn") || false;
+
+  const [userAccess, setUserAccess] = useState(
+    isLoggedIn === "true" ? true : false
+  );
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      axios
+        .get(`${process.env.REACT_APP_BACK_END_HOST}/validate`, {
+          withCredentials: true,
+          headers: {
+            "Access-Control-Allow-Origin": process.env.REACT_APP_FRONT_END_HOST,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
           if (res.data === "valid") {
             setUserAccess(true);
-            localStorage.setItem('isLoggedIn', true);
+            localStorage.setItem("isLoggedIn", true);
           } else {
             setUserAccess(false);
-            localStorage.removeItem('isLoggedIn');
+            localStorage.removeItem("isLoggedIn");
           }
         })
-        .catch(err => console.error(err))
-      }
-  }, [])
+        .catch((err) => console.error(err));
+    }
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <Header 
-          userAccess={userAccess}
-          setUserAccess={setUserAccess}
-        />
+        <Speech />
+        <Header userAccess={userAccess} setUserAccess={setUserAccess} />
         {!userAccess && <Auth />}
         {/* <Pull /> */}
         {userAccess && <TweetList />}
