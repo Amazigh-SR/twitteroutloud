b48854319cdef3d71dfa3977ed789fb95cfbf5f9
Amazigh-SR <s_Red009@hotmail.com>
2021-04-11 12:58:59 -0400

    Create main tts funtion
    Co-authored-by: souredoutlook <souredoutlook@gmail.com>
    Co-authored-by: Dan Pappo <dpappo@gmail.com>

---
+++ MyApp/src/helpers/speechSynthesis.js
@@ -0,0 +1,41 @@
+// Import mock data - tested and validated
+const mockData = require("./mockData");
+const checkSpeechSynthesis = require("./checkSpeechSynthesis");
+const fetchEnVoices = require("./fetchEnVoices");
+const speechSettings = require("./speechSettings");
+
+//-----------------------------------------------//
+
+const speechSynthesis = function (tweetObject) {
+  const tweetText = tweetObject.full_text;
+  const name = tweetObject.user.name;
+
+  const finalFormMessage = `${name} tweets ${tweetText}`;
+
+  //Check browser support
+  if (!checkSpeechSynthesis()) {
+    return "Sorry this browser does not support speechSynthesis";
+  }
+
+  //Setup synthesis object
+  const synthesis = window.speechSynthesis;
+
+  //Fetch english voices
+  const voices = fetchEnVoices(synthesis);
+
+  //Generate the voice for a given text
+  const utterance = new SpeechSynthesisUtterance(finalFormMessage);
+
+  //Static settings
+  utterance.voice = voices[0];
+  utterance.pitch = 1.5;
+  utterance.rate = 1;
+  utterance.volume = 0.8;
+
+  // Function that calls the speak method to generate audio for a given message
+  synthesis.speak(utterance);
+};
+
+// speechSynthesis(mockData[0]);
+
+exports.speechSynthesis = speechSynthesis;
