9c1f5f3e1c2139192f20d41470faea94edad1d44
Amazigh-SR <s_Red009@hotmail.com>
2021-04-12 14:49:09 -0400

    Implement state for settings & fix big in cb func
    Co-authored-by: souredoutlook <souredoutlook@gmail.com>
    Co-authored-by: Dan Pappo <dpappo@gmail.com>

--- MyApp/src/components/Settings.jsx
+++ MyApp/src/components/Settings.jsx
@@ -1,44 +1,71 @@
-import {useState} from 'react'
+import "../App.css";
 
 export default function Settings(props) {
-  
-  const [settings, setSettings] = useState({
-    voice: 1,
-    volume: 1,
-    rate: 1,
-    pitch: 1
-  })
+  console.log("props: ", props);
 
   function handleChange(evt) {
-    const value = evt.target.value;
-    setSettings({
-      ...settings,
-      [evt.target.name]: value
-    })
+    const value = Number(evt.target.value);
+    props.setSettings((prev) => ({
+      ...prev,
+      [evt.target.name]: parseFloat(value),
+    }));
   }
 
-  
-  return <div className="settingsComponent">
+  return (
+    <div className="settingsComponent">
+      <div class="option">
+        <label for="voice">Voice</label>
+        <select name="voice" id="voice" onChange={handleChange}>
+          <option value="0">NameA</option>
+          <option value="1">NameB</option>
+          <option value="2">NameC</option>
+          <option value="3">NameD</option>
+          <option value="4">NameE</option>
+          <option value="5">NameF</option>
+        </select>
+      </div>
 
-  <div class="option">
-		<label for="voice">Voice</label>
-		<select name="voice" id="voice"></select>
-	</div>
+      <div class="option">
+        <label for="volume">Volume</label>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          name="volume"
+          id="volume"
+          value={props.settings.volume}
+          onChange={handleChange}
+        />
+      </div>
 
-	<div class="option">
-		<label for="volume">Volume</label>
-		<input type="range" min="0" max="1" step="0.1" name="volume" id="volume" value={settings.volume} onChange={handleChange}/>
-	</div>
+      <div class="option">
+        <label for="rate">Rate</label>
+        <input
+          type="range"
+          min="0.1"
+          max="10"
+          step="0.1"
+          name="rate"
+          id="rate"
+          value={props.settings.rate}
+          onChange={handleChange}
+        />
+      </div>
 
-	<div class="option">
-		<label for="rate">Rate</label>
-		<input type="range" min="0.1" max="10" step="0.1" name="rate" id="rate" value={settings.rate} onChange={handleChange}/>
-	</div>
-
-	<div class="option">
-		<label for="pitch">Pitch</label>
-		<input type="range" min="0" max="2" step="0.1" name="pitch" id="pitch" value={settings.pitch} onChange={handleChange}/>
-	</div>
-
-  </div>
+      <div class="option">
+        <label for="pitch">Pitch</label>
+        <input
+          type="range"
+          min="0"
+          max="2"
+          step="0.1"
+          name="pitch"
+          id="pitch"
+          value={props.settings.pitch}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
 }
