050102e0fd05cd78023bf9e1990e1261d7955209
Amazigh-SR <s_Red009@hotmail.com>
2021-04-12 14:49:59 -0400

    Insert logs to test potential issues
    Co-authored-by: souredoutlook <souredoutlook@gmail.com>
    Co-authored-by: Dan Pappo <dpappo@gmail.com>

--- MyApp/src/components/Speech.jsx
+++ MyApp/src/components/Speech.jsx
@@ -1,61 +1,80 @@
 // import checkSpeechSynthesis from "../helpers/checkSpeechSynthesis";
 // import fetchEnVoices from "../helpers/fetchEnVoices";
+
 import { speechSynthesis } from "../helpers/speechSynthesis";
+import Settings from "./Settings";
 import mockData from "../helpers/mockData";
+import { useState, Fragment, useEffect } from "react";
 
 export default function Speech(props) {
   const { tweets, setTweets } = props;
-  
-  let currentTweet = 0
-  
-  const playSpeech = function() {
+  const [settings, setSettings] = useState({
+    voiceId: 1,
+    volume: 5,
+    rate: 1,
+    pitch: 1,
+  });
+
+  let currentTweet = 0;
+  console.log("SETTINGS---->", settings);
+  const playSpeech = function (settings) {
     for (let i = 0; i < tweets.length; i++) {
-      window.speechSynthesis.resume() || speechSynthesis(tweets[i])
+      window.speechSynthesis.resume() || speechSynthesis(tweets[i], settings);
+      // speechSynthesis(tweets[i], settings);
     }
   };
 
-  const pauseSpeech = function() {
-    window.speechSynthesis.pause()
-    };
+  useEffect(() => {
+    console.log("TYPE OF WINDOW", typeof window, window.speechSynthesis);
+  }, []);
 
-  const stopSpeech = function() {
-    window.speechSynthesis.cancel()
-    };
+  const pauseSpeech = function () {
+    window.speechSynthesis.pause();
+  };
 
-  const nextTweet = function() {
+  const stopSpeech = function () {
     window.speechSynthesis.cancel();
-    currentTweet++
-    speechSynthesis(tweets[currentTweet])
-    };
+  };
+
+  const nextTweet = function () {
+    window.speechSynthesis.cancel();
+    currentTweet++;
+    speechSynthesis(tweets[currentTweet], settings);
+  };
 
   return (
-    <div class="btn-toolbar mb-3 speechComponent" role="toolbar" aria-label="Toolbar with button groups">
-      <div class="btn-group mr-2" role="group" aria-label="First group">
-      {/* <button onClick={()=>onClick()}>Previous</button> */}
-      <button type="button" className="btn player"
-      onClick={()=>playSpeech()}>
-        <i class="bi bi-play-fill"></i>
-      </button>
-      <button className="btn player" 
-        onClick={()=>pauseSpeech()}>
-          <i class="bi bi-pause"></i>
-      </button>
-      <button className="btn player" 
-        onClick={()=>stopSpeech()}>
-          <i class="bi bi-stop-fill"></i>
-      </button>
-      <button className="btn player" 
-        onClick={()=>nextTweet()}>
-          <i class="bi bi-skip-end-fill"></i>
-      </button>
-    </div>
-    <div className="btn-group">
-    <button className="btn player"
-      onClick={""}>Settings</button>
-    </div>
-  </div>
+    <>
+      <div
+        class="btn-toolbar mb-3 speechComponent"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
+        <div class="btn-group mr-2" role="group" aria-label="First group">
+          {/* <button onClick={()=>onClick()}>Previous</button> */}
+          <button
+            type="button"
+            className="btn player"
+            onClick={() => playSpeech(settings)}
+          >
+            <i class="bi bi-play-fill"></i>
+          </button>
+          <button className="btn player" onClick={() => pauseSpeech()}>
+            <i class="bi bi-pause"></i>
+          </button>
+          <button className="btn player" onClick={() => stopSpeech()}>
+            <i class="bi bi-stop-fill"></i>
+          </button>
+          <button className="btn player" onClick={() => nextTweet()}>
+            <i class="bi bi-skip-end-fill"></i>
+          </button>
+        </div>
+        <div className="btn-group">
+          <button className="btn player" onClick={""}>
+            Settings
+          </button>
+        </div>
+      </div>
+      <Settings settings={settings} setSettings={setSettings} />
+    </>
   );
 }
-
-//SpeechSynthesis.pause()
-//Puts the SpeechSynthesis object into a paused state.
