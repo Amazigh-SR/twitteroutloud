ff457b0c6b870305e4a3b1b87aed285529756e7e
souredoutlook <souredoutlook@gmail.com>
2021-04-12 23:16:02 -0400

    add gradient to loading anim

--- MyApp/src/components/Loading.jsx
+++ MyApp/src/components/Loading.jsx
@@ -4,8 +4,8 @@ export default function Loading(props) {
   return (
     <>
       <img className="circle loading top" src="twitter_bird.svg" alt="twitter bird" />
-        <div className="loading">{props.children}</div>
-      {/* <img className="circle loading bottom" src="twitter_bird_flipped.svg" alt="twitter bird" />  */}
+        <h1 className="loading linear-wipe">{props.children}</h1>
+      <img className="circle loading bottom" src="twitter_bird_flipped.svg" alt="twitter bird" /> 
     </>
   )
 
