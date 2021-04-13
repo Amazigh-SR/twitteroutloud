f0dd47b3aef2690e7f2a6104fce031584454b00a
Dan Pappo <dpappo@gmail.com>
2021-04-13 16:09:39 -0400

    add listener for last tweet read

--- MyApp/src/hooks/usePlayerControls.js
+++ MyApp/src/hooks/usePlayerControls.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef } from "react";
 
 const speech = window.speechSynthesis;
 
@@ -8,24 +8,24 @@ const playerConstants = {
   PAUSE: "PAUSE",
   PREV: "PREV",
   NEXT: "NEXT",
-  RESUME: "RESUME"
-}
+  RESUME: "RESUME",
+};
 
 const { PLAY, STOP, RESUME, PAUSE, PREV, NEXT } = playerConstants;
 
-const usePlayer = function(utterances) {
+const usePlayer = function (utterances) {
   const [mode, setMode] = useState(null);
-  const [tracks, setTracks] = useState([...utterances])
+  const [tracks, setTracks] = useState([...utterances]);
   const [nextTrack, setNextTrack] = useState(0);
 
   for (const utterance of utterances) {
-    utterance.onstart = ()=>setNextTrack(prev => prev + 1);
-  };
+    utterance.onstart = () => setNextTrack((prev) => prev + 1);
+  }
 
-  const play = function(settings, previous = false) {
+  const play = function (settings, previous = false) {
     const { voice, pitch, rate, volume } = settings;
-    
-    setMode(PLAY)
+
+    setMode(PLAY);
 
     let startingTrack = nextTrack;
 
@@ -34,60 +34,63 @@ const usePlayer = function(utterances) {
     }
 
     for (let i = startingTrack; i < tracks.length; i++) {
-
       tracks[i].voice = voice;
       tracks[i].pitch = pitch;
       tracks[i].rate = rate;
       tracks[i].volume = volume;
-      
-      speech.speak(tracks[i]);
-    }
-  }
 
-  const resume = function() {
+      speech.speak(tracks[i]);
+
+      if (i === tracks.length - 1) {
+        tracks[i].addEventListener("end", function (event) {
+          console.log("Finished reading loaded tweets");
+        });
+      }
+    }
+  };
+
+  const resume = function () {
     setMode(RESUME);
 
     speech.resume();
   };
 
-  const pause = function() {
+  const pause = function () {
     setMode(PAUSE);
 
     speech.pause();
   };
-  
-  const stop = function() {
+
+  const stop = function () {
     setMode(STOP);
 
     speech.cancel();
 
     setNextTrack(0);
   };
-  
-  const previous = function(settings) {
+
+  const previous = function (settings) {
     setMode(PLAY);
 
     speech.cancel();
 
     if (nextTrack > 1) {
-      setNextTrack(prev => prev - 2);
-      play(settings, true)
+      setNextTrack((prev) => prev - 2);
+      play(settings, true);
     }
-
   };
 
-  const next = function(settings) {
+  const next = function (settings) {
     setMode(PLAY);
-    
+
     speech.cancel();
 
     if (nextTrack <= tracks.length - 1) {
-      play(settings)
+      play(settings);
     }
-
   };
 
-   return {
+  return {
     mode,
     play,
     resume,
@@ -96,7 +99,7 @@ const usePlayer = function(utterances) {
     previous,
     next,
     currentUtterance: nextTrack - 1,
-  }
-}
+  };
+};
 
-export { usePlayer, playerConstants }
+export { usePlayer, playerConstants };
