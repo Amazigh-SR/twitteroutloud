787669b95e28ace5f2b80ef5e050a120d9036401
Amazigh-SR <s_Red009@hotmail.com>
2021-04-13 23:48:04 -0400

    Implement useSpeechRecognition hook  & feature

--- MyApp/src/components/Speech.jsx
+++ MyApp/src/components/Speech.jsx
@@ -4,15 +4,18 @@ import { speechSynthesis } from "../helpers/speechSynthesis";
 import Settings from "./Settings";
 import mockData from "../helpers/mockData";
 import { useState, Fragment, useEffect } from "react";
-import fetchEnVoices from "../helpers/fetchEnVoices.js"
-import $ from 'jquery'
-import { usePlayer, playerConstants } from '../hooks/usePlayerControls';
+import fetchEnVoices from "../helpers/fetchEnVoices.js";
+import $ from "jquery";
+import { usePlayer, playerConstants } from "../hooks/usePlayerControls";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const { PAUSE } = playerConstants;
 
 export default function Speech(props) {
   let voices = fetchEnVoices(window.speechSynthesis);
-  
+
   const { tweets } = props;
   const [settings, setSettings] = useState({
     voice: window.speechSynthesis.getVoices()[0],
@@ -21,7 +24,7 @@ export default function Speech(props) {
     pitch: 1,
   });
 
-  const utterances = tweets.map(tweet => speechSynthesis(tweet, settings));
+  const utterances = tweets.map((tweet) => speechSynthesis(tweet, settings));
 
   const {
     mode,
@@ -33,8 +36,7 @@ export default function Speech(props) {
     next,
     currentUtterance,
   } = usePlayer(utterances);
-  
-  
+
   let currentTweet = 0;
   const playSpeech = function (settings) {
     for (let i = 0; i < tweets.length; i++) {
@@ -42,7 +44,7 @@ export default function Speech(props) {
       // speechSynthesis(tweets[i], settings);
     }
   };
-  
+
   useEffect(() => {
     $("#settingsButton").click(() => {
       setSettings({
@@ -50,32 +52,55 @@ export default function Speech(props) {
         volume: 5,
         rate: 1,
         pitch: 1,
-      })
-      if ( $( "div.settingsComponent" ).first().is( ":hidden" ) ) {
-        $( "div.settingsComponent" ).slideDown( "fast" );
+      });
+      if ($("div.settingsComponent").first().is(":hidden")) {
+        $("div.settingsComponent").slideDown("fast");
       } else {
-        $( "div.settingsComponent" ).slideUp();
+        $("div.settingsComponent").slideUp();
       }
-    })
+    });
   }, []);
 
-  const pauseSpeech = function () {
-    window.speechSynthesis.pause();
-  };
+  useEffect(() => {}, [currentTweet]);
 
-  const stopSpeech = function () {
-    window.speechSynthesis.cancel();
-  };
+  // ------------------------------------------------------------- //
+  // Speech Recognition API is currently following a "push to talk" approach.
+  let speechListenerIsActive = false;
+  // const { resetTranscript } = useSpeechRecognition();
+  const commands = [
+    {
+      command: ["start"],
+      callback: () => play(settings),
+    },
 
-  const nextTweet = function () {
-    window.speechSynthesis.cancel();
-    currentTweet++;
-    speechSynthesis(tweets[currentTweet], settings);
-  };
+    {
+      command: ["stop"],
+      callback: stop,
+    },
+    {
+      command: ["pause"],
+      callback: pause,
+    },
+    {
+      command: ["resume"],
+      callback: resume,
+    },
 
-  useEffect(()=>{
+    {
+      command: ["Next tweet"],
+      callback: () => next(settings),
+    },
 
-  },[currentTweet])
+    {
+      command: ["Previous tweet "],
+      callback: () => previous(settings),
+    },
+  ];
+
+  const { transcript } = useSpeechRecognition({ commands });
+
+  // ------------------------------------------------------------ //
+
   return (
     <>
       <h1>Now playing: Tweet #{currentUtterance}</h1>
@@ -85,19 +110,28 @@ export default function Speech(props) {
         aria-label="Toolbar with button groups"
       >
         <div className="btn-group mr-2" role="group" aria-label="First group">
-          <button type="button"
+          <button
+            type="button"
             className="btn player"
-            onClick={()=>previous(settings)}>
+            onClick={() => previous(settings)}
+          >
             <i className="bi bi-skip-backward-fill"></i>
           </button>
           <button
             type="button"
             className="btn player"
-            onClick={() => { mode === PAUSE ? resume() : play(settings) }}
+            onClick={() => {
+              mode === PAUSE ? resume() : play(settings);
+            }}
           >
             <i className="bi bi-play-fill"></i>
           </button>
-          <button className="btn player" onClick={() => { pause() }}>
+          <button
+            className="btn player"
+            onClick={() => {
+              pause();
+            }}
+          >
             <i className="bi bi-pause"></i>
           </button>
           <button className="btn player" onClick={() => stop()}>
@@ -113,7 +147,19 @@ export default function Speech(props) {
           </button>
         </div>
       </div>
-      <Settings settings={settings} setSettings={setSettings} voices={voices}/>
+      <Settings settings={settings} setSettings={setSettings} voices={voices} />
+      <button
+        onClick={() => {
+          speechListenerIsActive = !speechListenerIsActive;
+          console.log(speechListenerIsActive);
+          SpeechRecognition.startListening({
+            continuous: speechListenerIsActive,
+          });
+        }}
+      >
+        Activate Voice Commands
+      </button>
+      <p id="transcript">Transcript: {transcript}</p>
     </>
   );
 }
