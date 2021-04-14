6c0cde287d78bc4c72551de6db9d6d225c63e663
Amazigh-SR <s_Red009@hotmail.com>
2021-04-14 02:28:06 -0400

    Include a description of VCs feature

--- MyApp/src/components/Auth.jsx
+++ MyApp/src/components/Auth.jsx
@@ -1,17 +1,23 @@
 export default function Auth() {
   return (
     <div className="auth-container">
-      <h2 key="header">Listen to your Twitter feed and give scrolling a break</h2>
+      <h2 key="header">
+        Listen to your Twitter feed and give scrolling a break
+      </h2>
       <ul key="list">
-      <li key="1">Tweets read by a real life computer</li>
-      <li key="2">Twitter themed</li>
-      <li key="3">Open sign-ups</li>
-      <li key="4">Full web support</li>
-      <li key="5">Open source</li>
-      <li key="6">Powered by coffee</li>
-
+        <li key="1">Tweets read by a real life computer</li>
+        <li key="2">Twitter themed</li>
+        <li key="3">Open sign-ups</li>
+        <li key="4">Full web support</li>
+        <li key="5">Open source</li>
+        <li key="6">Powered by coffee</li>
+        <li key="6">Play, pause and navigate with voice control </li>
       </ul>
-      <a key="link" href="http://localhost:3001/auth"><button className="btn btn-primary logout" ><i className="bi bi-twitter"></i> Sign in with Twitter</button></a>
+      <a key="link" href="http://localhost:3001/auth">
+        <button className="btn btn-primary logout">
+          <i className="bi bi-twitter"></i> Sign in with Twitter
+        </button>
+      </a>
     </div>
   );
 }
