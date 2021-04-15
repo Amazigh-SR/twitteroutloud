de4395e4db5f25e6fe07b84d62d989a1f124cae0
Amazigh-SR <s_Red009@hotmail.com>
2021-04-15 13:14:26 -0400

    Add class toggles for AVC & App mode drawers
    Co-authored-by: Dan Pappo <dpappo@gmail.com>

--- MyApp/src/components/Speech.jsx
+++ MyApp/src/components/Speech.jsx
@@ -183,7 +183,8 @@ export default function Speech(props) {
           className="btn player"
           id="voice-commands"
           onClick={() => {
-            // document.querySelector("#VC-drawer").classList.toggle(".invisible");
+            document.querySelector("#VC-drawer").classList.toggle("invisible");
+            document.querySelector("#VC-drawer").classList.toggle("drawer");
             const newSpeechListenerIsActive = !speechListenerIsActive;
             setSpeechListenerIsActive(!speechListenerIsActive);
 
@@ -255,10 +256,15 @@ export default function Speech(props) {
             id="settingsButton"
             className="btn player"
             onClick={() => {
-              props.getTweets();
+              document
+                .querySelector("#appMode-drawer")
+                .classList.toggle("invisible");
+              document
+                .querySelector("#appMode-drawer")
+                .classList.toggle("drawer");
             }}
           >
-            Load More Tweets
+            App Mode
           </button>
         </div>
       </div>
@@ -269,7 +275,7 @@ export default function Speech(props) {
         $={$}
       />
       {/* Create a component for list of commands to clean up this disasta */}
-      <div id="VC-drawer">
+      <div id="VC-drawer" className="invisible">
         <h2>List of Voice Commands</h2>
         <div className="VC-list">
           <div>
@@ -297,6 +303,27 @@ export default function Speech(props) {
         <p id="transcript">
           <strong>Transcript:</strong> {transcript}
         </p>
+      </div>
+      <div id="appMode-drawer" className="invisible">
+        {/* <h2>App Modes</h2> */}
+        <div className="mode-list">
+          <div>
+            {/* <ul> */}
+            <button className="btn btn-primary modeButton" onClick={() => {}}>
+              <i className="fas fa-infinity"></i>
+              Binge
+            </button>
+            {/* </ul> */}
+          </div>
+          <div>
+            {/* <ul> */}
+            <button className="btn btn-primary modeButton" onClick={() => {}}>
+              <i className="fas fa-stopwatch"></i>
+              Timer
+            </button>
+            {/* </ul> */}
+          </div>
+        </div>
       </div>
     </>
   );
