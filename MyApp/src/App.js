163c401917447321bb069b0462b5cd04508d8350
Dan Pappo <dpappo@gmail.com>
2021-04-17 12:59:18 -0400

    default mode binge

--- MyApp/src/App.js
+++ MyApp/src/App.js
@@ -24,7 +24,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [settings, setSettings] = useState({});
   const [voices, setVoices] = useState([]);
-  const [appMode, setAppMode] = useState(THREAD);
+  const [appMode, setAppMode] = useState(BINGE);
 
   // useEffect(() => {
   //   // if(MODE ==="Thread")
