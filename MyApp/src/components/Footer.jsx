384d865d504758ca0a35bf51fda1d0f4d9b571d3
Dan Pappo <dpappo@gmail.com>
2021-04-18 12:32:19 -0400

    footer position changed to inherit

--- MyApp/src/components/Footer.jsx
+++ MyApp/src/components/Footer.jsx
@@ -1,3 +1,3 @@
 export default function Footer(props) {
-  return (<div style={{position: "fixed", bottom: "0"}} className="footer"><p style={{fontSize: "0.4em"}}>Disclaimer: TwitterOutLoud is not affiliated with Twitter, Inc., and respects Twitter’s trademarks and does not claim them as our own. We at TwitterOutLoud are fans of Twitter. Check out our Twitter account, @aloud_tweets!</p></div>)
+  return (<div style={{position: "inherit", bottom: "0"}} className="footer"><p style={{fontSize: "0.4em"}}>Disclaimer: TwitterOutLoud is not affiliated with Twitter, Inc., and respects Twitter’s trademarks and does not claim them as our own. We at TwitterOutLoud are fans of Twitter. Check out our Twitter account, @aloud_tweets!</p></div>)
 }
