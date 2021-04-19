ebffdcbf5cfcf67587fe0caa3bdfeb5d9a4c6ca2
souredoutlook <souredoutlook@gmail.com>
2021-04-18 22:05:50 -0400

    add loading condition to App.js

--- MyApp/src/App.js
+++ MyApp/src/App.js
@@ -47,126 +47,124 @@ function App() {
     };
 
     window.addEventListener("onbeforeunload", handleWindowClose);
+      if (!isLoggedIn && loading) {
+        axios
+          .get(`${process.env.REACT_APP_BACK_END_HOST}/validate`, {
+            withCredentials: true,
+            headers: {
+              "Access-Control-Allow-Origin": process.env.REACT_APP_FRONT_END_HOST,
+            },
+          })
+          .then((res) => {
+            const { valid, dbSettings, profile } = res.data;
+            if (valid) {
+              const { username, image_url } = profile;
 
-    if (!isLoggedIn) {
-      axios
-        .get(`${process.env.REACT_APP_BACK_END_HOST}/validate`, {
-          withCredentials: true,
-          headers: {
-            "Access-Control-Allow-Origin": process.env.REACT_APP_FRONT_END_HOST,
-          },
-        })
-        .then((res) => {
-          const { valid, dbSettings, profile } = res.data;
-          if (valid) {
-            const { username, image_url } = profile;
-
-            setUserAccess(valid);
-            localStorage.setItem("isLoggedIn", true);
-            localStorage.setItem("username", username);
-            localStorage.setItem("image_url", image_url);
-            for (const [key, value] of Object.entries(dbSettings)) {
-              localStorage.setItem(key, value);
-            }
-            fetchEnVoices(
-              window.speechSynthesis,
-              setVoices,
-              setSettings,
-              dbSettings
-            );
-          } else {
-            setUserAccess(valid);
-            //!  we need to look at the error that this code is causing
-            // localStorage.removeItem("isLoggedIn");
-            // for (const [key] of Object.entries(settings)) {
-            //   localStorage.removeItem(key);
-            // }
-            // localStorage.removeItem("username");
-            // localStorage.removeItem("image_url");
-            setTimeout(() => setLoading(false), 1000);
-          }
-          return res.data;
-        })
-        .then((data) => {
-          if (data.valid) {
-            // IF BASED ON MODE:
-
-            return getTweets().then((tweets) => {
-              if (appMode === THREAD) {
-                threadHelper(tweets).then((res) => {
-                  console.log("res from threadHelper", res);
-                  setTweets(res);
-                  setTimeout(() => setLoading(false), 1000);
-                });
-              } else if (appMode === BINGE) {
-                //? commented out code below was prexisting master code => new if block handles rate limit on binge
-                // setTweets(tweets);
-                // setTimeout(() => setLoading(false), 1000);
-                if (
-                  tweets[0].message &&
-                  tweets[0].message === "Rate limit exceeded"
-                ) {
-                  setRateLimitExceeded(true);
-                  setTimeout(() => setLoading(false), 1000);
-                } else {
-                  setTweets(tweets);
-                  setTimeout(() => setLoading(false), 1000);
-                }
+              setUserAccess(valid);
+              localStorage.setItem("isLoggedIn", true);
+              localStorage.setItem("username", username);
+              localStorage.setItem("image_url", image_url);
+              for (const [key, value] of Object.entries(dbSettings)) {
+                localStorage.setItem(key, value);
               }
-            });
-
-            // threadHelper(tweets);
-            // setTweets(tweets);
-            // setTimeout(() => setLoading(false), 1000);
-            // });
-            //if x.setting is true setInterval
-          }
-        })
-        .catch((err) => console.error(err));
-    } else {
-      const localSettings = {
-        rate: Number(localStorage.getItem("rate")),
-        pitch: Number(localStorage.getItem("pitch")),
-        volume: Number(localStorage.getItem("volume")),
-        voice: localStorage.getItem("voice"),
-      };
-      fetchEnVoices(
-        window.speechSynthesis,
-        setVoices,
-        setSettings,
-        localSettings
-      );
-
-      // IF BASED ON MODE
-
-      getTweets()
-        .then((tweets) => {
-          if (appMode === THREAD) {
-            threadHelper(tweets).then((res) => {
-              console.log("res from threadHelper", res);
-              setTweets(res);
-              setTimeout(() => setLoading(false), 1000);
-            });
-          } else if (appMode === BINGE) {
-            //? commented out code below was prexisting master code => new if block handles rate limit on binge
-            // setTweets(tweets);
-            if (
-              tweets[0].message &&
-              tweets[0].message === "Rate limit exceeded"
-            ) {
-              setRateLimitExceeded(true);
-              setTimeout(() => setLoading(false), 1000);
+              fetchEnVoices(
+                window.speechSynthesis,
+                setVoices,
+                setSettings,
+                dbSettings
+              );
             } else {
-              setTweets(tweets);
+              setUserAccess(valid);
+              //!  we need to look at the error that this code is causing
+              // localStorage.removeItem("isLoggedIn");
+              // for (const [key] of Object.entries(settings)) {
+              //   localStorage.removeItem(key);
+              // }
+              // localStorage.removeItem("username");
+              // localStorage.removeItem("image_url");
               setTimeout(() => setLoading(false), 1000);
             }
-          }
-        })
-        .catch((err) => console.error(err));
-    }
+            return res.data;
+          })
+          .then((data) => {
+            if (data.valid) {
+              // IF BASED ON MODE:
 
+              return getTweets().then((tweets) => {
+                if (appMode === THREAD) {
+                  threadHelper(tweets).then((res) => {
+                    console.log("res from threadHelper", res);
+                    setTweets(res);
+                    setTimeout(() => setLoading(false), 1000);
+                  });
+                } else if (appMode === BINGE) {
+                  //? commented out code below was prexisting master code => new if block handles rate limit on binge
+                  // setTweets(tweets);
+                  // setTimeout(() => setLoading(false), 1000);
+                  if (
+                    tweets[0].message &&
+                    tweets[0].message === "Rate limit exceeded"
+                  ) {
+                    setRateLimitExceeded(true);
+                    setTimeout(() => setLoading(false), 1000);
+                  } else {
+                    setTweets(tweets);
+                    setTimeout(() => setLoading(false), 1000);
+                  }
+                }
+              });
+
+              // threadHelper(tweets);
+              // setTweets(tweets);
+              // setTimeout(() => setLoading(false), 1000);
+              // });
+              //if x.setting is true setInterval
+            }
+          })
+          .catch((err) => console.error(err));
+      } else if (isLoggedIn && loading){
+        const localSettings = {
+          rate: Number(localStorage.getItem("rate")),
+          pitch: Number(localStorage.getItem("pitch")),
+          volume: Number(localStorage.getItem("volume")),
+          voice: localStorage.getItem("voice"),
+        };
+        fetchEnVoices(
+          window.speechSynthesis,
+          setVoices,
+          setSettings,
+          localSettings
+        );
+
+        // IF BASED ON MODE
+
+        getTweets()
+          .then((tweets) => {
+            if (appMode === THREAD) {
+              threadHelper(tweets).then((res) => {
+                console.log("res from threadHelper", res);
+                setTweets(res);
+                setTimeout(() => setLoading(false), 1000);
+              });
+            } else if (appMode === BINGE) {
+              //? commented out code below was prexisting master code => new if block handles rate limit on binge
+              // setTweets(tweets);
+              if (
+                tweets[0].message &&
+                tweets[0].message === "Rate limit exceeded"
+              ) {
+                setRateLimitExceeded(true);
+                setTimeout(() => setLoading(false), 1000);
+              } else {
+                setTweets(tweets);
+                setTimeout(() => setLoading(false), 1000);
+              }
+            }
+          })
+          .catch((err) => console.error(err));
+      }
     return window.removeEventListener("onbeforeunload", handleWindowClose);
-  }, [appMode]);
+  }, [appMode, loading]);
 
   return (
     <div className="App">
